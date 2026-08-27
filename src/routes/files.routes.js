6d7c72eac3f34f5a113e232faf4fb4ee7defcacf
Ayan Manna <ayanmanna858@gmail.com>
2026-08-27 12:35:54 +0530

    Day-13

--- src/routes/files.routes.js
+++ src/routes/files.routes.js
@@ -1,24 +1,16 @@
 const express = require('express');
-const multer = require('multer');
 const filesController = require('../controllers/files.controller');
 const { protect } = require('../middlewares/auth.middleware');
 
 const router = express.Router();
-const upload = multer({
-  storage: multer.memoryStorage(),
-  limits: { fileSize: 50 * 1024 * 1024 }, // 50MB Telegram Bot API limit
-});
 
 // All file routes require authentication
 router.use(protect);
 
 router.post('/init', filesController.initFileUpload);
 router.post('/complete', filesController.completeFileUpload);
-router.post('/upload-telegram', upload.single('file'), filesController.uploadTelegramFile);
 router.get('/recent', filesController.getRecentFiles);
 router.get('/:id', filesController.getFile);
-router.get('/:id/view', filesController.viewFile);
-
 router.patch('/:id', filesController.updateFile);
 router.delete('/:id', filesController.deleteFile);
 router.post('/:id/copy', filesController.copyFile);
@@ -28,4 +20,3 @@ router.get('/:id/versions', filesController.getFileVersions);
 router.post('/:id/versions/restore', filesController.restoreFileVersion);
 
 module.exports = router;
-
